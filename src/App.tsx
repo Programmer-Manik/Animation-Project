1b528122b35157714912b55c32162e849b3921d3
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-19 23:35:29 +0600

    create button and 3Danimation add

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-
-import './App.css'
+import './button.css'
+// import './App.css'
 function App() {
 
   return (
     <div className='main'>
-      <div className='box-1'></div>
-      <div className='box-2'></div>
+      {/* <div className='box-1'></div> */}
+      {/* <div className='box-2'></div> */}
       {/* <h1>Md Manik Hossain</h1> */}
+      <button>submit</button>
     </div>
   )
 }
