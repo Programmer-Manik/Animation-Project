b53f3528a42855c258b3550ed77cd107f638de3a
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-20 01:48:20 +0600

    done loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import './box.css'
+import Loading from './Loading'
+// import './box.css'
 // import './button.css'
 // import './App.css'
 function App() {
 
   return (
-    <div className='main'>
+    <div>
+      <Loading></Loading>
       {/* <div className='box-1'></div> */}
       {/* <div className='box-2'></div> */}
       {/* <h1>Md Manik Hossain</h1> */}
       {/* <button className='btn'>submit</button> */}
-      <div className='box'></div>
+      {/* <div className='box'></div> */}
     </div>
   )
 }
