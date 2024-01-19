cdfee9ffc33f962b3037c20a7536278f66fa2bf1
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-19 23:09:59 +0600

    transform origin ex...

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className='main'>
       <div className='box-1'></div>
+      <div className='box-2'></div>
+      {/* <h1>Md Manik Hossain</h1> */}
     </div>
   )
 }
